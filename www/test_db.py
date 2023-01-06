e7369ddd867a668625ea4e39beab6e8f45c50f53
Vincentli081100 <121912653+Vincentli081100@users.noreply.github.com>
2023-01-06 13:04:35 +1300

    Try to eliminate the RuntimeError,but fail

--- www/test_db.py
+++ www/test_db.py
@@ -6,13 +6,17 @@ async def test(loop):
     #await orm.create_pool(user='www-data', password='www-data', db='awesome',loop=loop)
     await orm.create_pool(user='root', password='Leevis991224', db='awesome',loop=loop)
 
-    u = User(name='Test3', email='test3@example.com', passwd='1234567890', image='about:blank')
+    u = User(name='Test6', email='test6@example.com', passwd='1234567890', image='about:blank')
 
     await u.save()
 
 if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
+
+    #tasks = [test(loop), test_get(loop)]
+
+    #loop.run_until_complete(asyncio.wait(test(loop)))
 
     loop.run_until_complete(test(loop))
 
